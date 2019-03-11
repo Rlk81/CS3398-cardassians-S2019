670b5ca36c51ded3f7d8084c3d3619ae5c863c95
bsc52 <46979919+bsc52@users.noreply.github.com>
2019-03-10 21:17:16 -0500

    Added files for start menu

---
+++ Start_menu.h
@@ -0,0 +1,25 @@
+
+#ifndef _START_MENU_
+#define _START_MENU_
+
+// ------ Includes -----
+
+#include "IO.h"
+
+
+// --------------------------------------------------------------------------------
+//									 Start Menu
+// --------------------------------------------------------------------------------
+class Start_menu{
+public:
+	
+	
+	void Start_Menu(IO startIO);
+	
+
+
+	
+};
+
+#endif // _START_MENU_
+
